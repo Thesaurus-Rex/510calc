85c6d48de68f74a63520a16442c6349297435174
Aster Devi <wolfe5@psu.ds.pdx.edu>
2019-05-11 15:29:22 -0700

    Add project files.

---
+++ ReversePolishCalculator.UnitTests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ReversePolishCalculator
+{
+    [ExcludeFromCodeCoverage]
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Calculator_Add_Success()
+        {
+            var calc = new Calculator();
+            var first = 12;
+            var second = 12;
+            var result = calc.Add(first, second);
+            result.Should().Be(24, "Because addition...");
+        }
+    }
+}
